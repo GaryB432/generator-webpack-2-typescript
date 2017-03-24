11bbb929877c7ace095352062566d46010ea003b
Gary Bortosky <garybort@gmail.com>
2017-03-24 17:01:41 -0500

    hmr ftw

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -2,10 +2,6 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
 
 module.exports = {
-  devServer: {
-    outputPath: path.join(__dirname, 'dist'),
-    colors: false
-  },
   devtool: 'source-map',
   entry: {
     app: ['./src/app.ts']
@@ -30,6 +26,6 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([{from: 'src/public'}])],
   resolve: {
-    extensions: ['.ts']
+    extensions: ['.ts', '.js']
   }
 };
