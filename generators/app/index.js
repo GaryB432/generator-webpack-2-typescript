5f21bfc3b69782af4d1fc982e8b61330786b5d2e
Gary Bortosky <garybort@gmail.com>
2017-08-20 11:14:56 -0500

    some reformatting

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,9 +8,13 @@ const Case = require('case');
 module.exports = class extends Generator {
   initializing() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the minimal ' + chalk.red('Webpack TypeScript') + ' generator!'
-    ));
+    this.log(
+      yosay(
+        'Welcome to the minimal ' +
+        chalk.red('Webpack TypeScript') +
+        ' generator!'
+      )
+    );
 
     this.log(chalk.gray('Coming right up'));
 
@@ -19,27 +23,93 @@ module.exports = class extends Generator {
 
   writing() {
     const context = {appname: Case.kebab(this.cwd)};
-    this.fs.copy(this.templatePath('_vscode/settings.json'), this.destinationPath('.vscode/settings.json'));
-    this.fs.copy(this.templatePath('_vscode/tasks.json'), this.destinationPath('.vscode/tasks.json'));
-    this.fs.copy(this.templatePath('src/public/index.html'), this.destinationPath('src/public/index.html'));
-    this.fs.copyTpl(this.templatePath('src/scripts/app.ts'), this.destinationPath('src/scripts/app.ts'), context);
-    this.fs.copy(this.templatePath('src/scripts/greeter.spec.ts'), this.destinationPath('src/scripts/greeter.spec.ts'));
-    this.fs.copy(this.templatePath('src/scripts/greeter.ts'), this.destinationPath('src/scripts/greeter.ts'));
-    this.fs.copy(this.templatePath('src/styles/base.scss'), this.destinationPath('src/styles/base.scss'));
-    this.fs.copy(this.templatePath('webpack/dev-server.js'), this.destinationPath('webpack/dev-server.js'));
-    this.fs.copy(this.templatePath('webpack/entry.js'), this.destinationPath('webpack/entry.js'));
-    this.fs.copy(this.templatePath('webpack/module.js'), this.destinationPath('webpack/module.js'));
-    this.fs.copy(this.templatePath('webpack/output.js'), this.destinationPath('webpack/output.js'));
-    this.fs.copy(this.templatePath('webpack/plugins.js'), this.destinationPath('webpack/plugins.js'));
-    this.fs.copy(this.templatePath('webpack/resolve.js'), this.destinationPath('webpack/resolve.js'));
-    this.fs.copy(this.templatePath('_gitignore'), this.destinationPath('.gitignore'));
-    this.fs.copy(this.templatePath('karma-shim.js'), this.destinationPath('karma-shim.js'));
-    this.fs.copy(this.templatePath('karma.conf.js'), this.destinationPath('karma.conf.js'));
-    this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'), context);
-    this.fs.copyTpl(this.templatePath('README.md'), this.destinationPath('README.md'), context);
-    this.fs.copy(this.templatePath('tsconfig.json'), this.destinationPath('tsconfig.json'));
-    this.fs.copy(this.templatePath('tslint.json'), this.destinationPath('tslint.json'));
-    this.fs.copy(this.templatePath('webpack.config.js'), this.destinationPath('webpack.config.js'));
+    this.fs.copy(
+      this.templatePath('_vscode/settings.json'),
+      this.destinationPath('.vscode/settings.json')
+    );
+    this.fs.copy(
+      this.templatePath('_vscode/tasks.json'),
+      this.destinationPath('.vscode/tasks.json')
+    );
+    this.fs.copy(
+      this.templatePath('src/public/index.html'),
+      this.destinationPath('src/public/index.html')
+    );
+    this.fs.copyTpl(
+      this.templatePath('src/scripts/app.ts'),
+      this.destinationPath('src/scripts/app.ts'),
+      context
+    );
+    this.fs.copy(
+      this.templatePath('src/scripts/greeter.spec.ts'),
+      this.destinationPath('src/scripts/greeter.spec.ts')
+    );
+    this.fs.copy(
+      this.templatePath('src/scripts/greeter.ts'),
+      this.destinationPath('src/scripts/greeter.ts')
+    );
+    this.fs.copy(
+      this.templatePath('src/styles/base.scss'),
+      this.destinationPath('src/styles/base.scss')
+    );
+    this.fs.copy(
+      this.templatePath('webpack/dev-server.js'),
+      this.destinationPath('webpack/dev-server.js')
+    );
+    this.fs.copy(
+      this.templatePath('webpack/entry.js'),
+      this.destinationPath('webpack/entry.js')
+    );
+    this.fs.copy(
+      this.templatePath('webpack/module.js'),
+      this.destinationPath('webpack/module.js')
+    );
+    this.fs.copy(
+      this.templatePath('webpack/output.js'),
+      this.destinationPath('webpack/output.js')
+    );
+    this.fs.copy(
+      this.templatePath('webpack/plugins.js'),
+      this.destinationPath('webpack/plugins.js')
+    );
+    this.fs.copy(
+      this.templatePath('webpack/resolve.js'),
+      this.destinationPath('webpack/resolve.js')
+    );
+    this.fs.copy(
+      this.templatePath('_gitignore'),
+      this.destinationPath('.gitignore')
+    );
+    this.fs.copy(
+      this.templatePath('karma-shim.js'),
+      this.destinationPath('karma-shim.js')
+    );
+    this.fs.copy(
+      this.templatePath('karma.conf.js'),
+      this.destinationPath('karma.conf.js')
+    );
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      context
+    );
+    this.fs.copyTpl(
+      this.templatePath('README.md'),
+      this.destinationPath('README.md'),
+      context
+    );
+    this.fs.copy(
+      this.templatePath('tsconfig.json'),
+      this.destinationPath('tsconfig.json')
+    );
+    this.fs.copy(
+      this.templatePath('tslint.json'),
+      this.destinationPath('tslint.json')
+    );
+    this.fs.copy(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
+    );
   }
 
   install() {
